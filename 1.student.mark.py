c767d1897ad17c1d955a9e37e6082b6ed94186fd
khanggthai <75533198+khanggthai@users.noreply.github.com>
2021-03-24 11:40:51 +0700

    Update 1.student.mark.py

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,7 +1,17 @@
-student = {
-    "name" = name,
-    "id" = id,
-    "dob" = dob
-}
-name = input("name")
-student.update({"name": name})
+def studentcount():
+    count= int(input("How many?"))
+    return count
+
+def inputstudentinfo():
+    print("Enter student info:")
+    id = input("- ID")
+    name=
+    dob=
+    student ={ "id": id, "name": name, "dob": dob}
+    return student
+
+count = studentcout()
+students = []
+for i in range (0, count):
+    s = inputstudentinfo()
+    students += [s]
