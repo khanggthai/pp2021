94548185301be5ae06b45d3f2b6d682c529e312e
khanggthai <75533198+khanggthai@users.noreply.github.com>
2021-04-26 02:36:06 +0700

    Update 1.student.mark.py

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,21 +1,22 @@
 Student = {
-    "Id": "",
+    "Id": int,
     "Name": "",
     "DoB": ""
 }
 Course = {
-    "Id": "",
+    "Id": int,
     "Name": "",
     "Mark": []
 }
 Mark = {
-    "Name": "",
-    "Mark": -1.0
+    "Name": [],
+    "Mark": []
 }
 c1ass = []
 courses = []
-    
-def addstudentinfo(c1ass):
+
+
+def add_student_info(c1ass):
     print("Enter student info:")
     id = input("ID: ")
     Student.update({"Id": id})
@@ -23,73 +24,66 @@ def addstudentinfo(c1ass):
     Student.update({"Name": name})
     dob = input("Date of birth:")
     Student.update({"DoB": dob})
-    student_copy= Student.copy()
+    student_copy = Student.copy()
     c1ass.append(student_copy)
     print("This student is added")
 
-def addcourseinfo(courses):
+
+def add_course_info(courses):
     print("Enter course info")
     id = input("ID:")
     Course.update({"Id": id})
     name = input("Name:")
-    Course.update({ "Name": name})
+    Course.update({"Name": name})
     course_copy = Course.copy()
     courses.append(course_copy)
     print("This course is added")
 
-def displaystudent(c1ass):
+
+def display_student(c1ass):
     print(f"There are {len(c1ass)} students.")
     for i in c1ass:
         print(i.get("Id") + " : " + i.get("Name"))
 
-def displaycourses(courses):
+
+def display_courses(courses):
     print(f"There are {len(courses)} courses.")
     for s in courses:
         print(s.get("Id") + " : " + s.get("Name"))
 
-def enrollcourse(c1ass, courses):
-    print("Which course are they enrolling for?")
-    displaycourses(courses)
 
-    not_found_course = True
-    courseId = (str)(input())
-    while not_found_course:
-        for c in courses:
-            if c.get("Id") == courseId:
-                not_found_course = False
-                break
-        if (not_found_course):
-            print("Course not found! Try another one?")
-            n = input()
+def mark_student(c1ass, courses):
+    print("Which course?")
+    display_courses(courses)
+    courseId = int(input())
+    for c in courses:
+        if c.get("Id") == (courseId - 1):
+            display_student(c1ass)
+            studentId = int(input("Which student?"))
+            while studentId != 0:
+                for s in c1ass:
+                    if s.get("Id") == studentId:
+                        Mark.update({"Name": s.get("Name")})
+                        ["Mark"].append(Mark)
+        else: print("oofs")
 
-    print("Who will enroll in this course")
-    print("0 : No one.")
-    displaystudent(c1ass)
 
-    studentId = (str)(input())
-    while studentId != 0:
-        for s in c1ass:
-            if s.get("Id") == studentId:
-                Mark.update({"Name": s.get("Name")})
-                ["Mark"].append(Mark)
-        print("Who else will enroll in this course")
-        print("0 : No one else.")
-        displaystudent(c1ass)
-        studentId = (str)(input())
-#a
+# run
 
 count1 = int(input("How many students"))
 for i in range(count1):
-    print("The student number", i+1,"information:")
-    addstudentinfo(c1ass)
+    print("The student number", i + 1, "information:")
+    add_student_info(c1ass)
 
 count2 = int(input("How many courses?"))
 for i in range(count2):
-    print("The course number", i+1, "info:")
-    addcourseinfo(courses)
+    print("The course number", i + 1, "info:")
+    add_course_info(courses)
+
 
 def options():
-    print("""Choose?
+    print("""
+    Choose?
     1. Add a student
     2. Add a course
     3. List students in class
@@ -105,15 +99,15 @@ def student_management():
     while True:
         option = options()
         if option == 1:
-            addstudentinfo(c1ass)
+            add_student_info(c1ass)
         elif option == 2:
-            addcourseinfo(courses)
+            add_course_info(courses)
         elif option == 3:
-            displaystudent(c1ass)
+            display_student(c1ass)
         elif option == 4:
-            displaycourses(courses)
+            display_courses(courses)
         elif option == 5:
-            enrollcourse(c1ass, courses)
+            mark_student(c1ass, courses)
         else:
             print("Try again")
 
